f68ca3c48a187de5237965242b7cddc2a842af02
francisvalguedes <francisval.guedes.soares.037@ufrn.edu.br>
2025-02-12 09:55:11 -0300

    redundant files

--- get_text.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pygettext3 -d 00_Simplified -o locales/00_Simplified.pot source/pages/00_Simplified.py
-pygettext3 -d 00_Simplified -o locales/00_Simplified.pot source/pages/00_Simplified.py
